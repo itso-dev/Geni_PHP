cad2693c4aeba43149d8a3ac6c5a528301f15e35
jiwon <jw.kim@itso.co.kr>
2023-06-23 18:42:13 +0900

    update 정보입력/완료 페이지

---
+++ apply_completion.php
@@ -0,0 +1,41 @@
+<?php
+include_once('head.php');
+?>
+
+
+<link rel="stylesheet" type="text/css" href="css/apply.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="css/reset.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<body>
+    <div>
+        <header id="apply-header">
+            <img src="img/apply/logo.blue.png" />
+        </header>
+
+        <section id="apply-container">
+            <article class="apply-page apply-completion max">
+                <p class="title">i.M 정규직 드라이버
+                    <span>
+                        '지니’<br />
+                        입사 지원이 완료되었습니다.
+                    </span>
+                </p>
+                <p class="sub-title">
+                    채용 담당자가 근무일 기준 3일 이내로 면접 진행 관련해서 안내를 드릴 예정입니다.<br /><br class="mo-br768" />
+                    감사합니다.
+                </p>
+                <div class="submit-btn">
+                    완료
+                </div>
+                <img src="img/apply/im.png" class="car" />
+            </article>
+        </section>
+    </div>
+
+
+    <?php
+    include_once('tale.php');
+    ?>
